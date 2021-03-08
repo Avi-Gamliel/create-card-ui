40e78eb1c7fd1a46f60c9f5bf1fcd07b848a35e4
avi gamliel <gamlielphoto@gmail.com>
2021-03-08 19:49:33 +0200

    add flip functions

--- README.md
+++ README.md
@@ -10,30 +10,28 @@ And...
 
 ```
 
-import {makeCardUi} from 'simple-game-card-ui'
+import { makeCardUi, makeBackCardUi, createCardFlip, flipCard, addBack } from 'simple-game-card-ui'
 import 'simple-game-card-ui/style.css';
 
 ```
-
 
 # Create card
 
 First we create a simple element with id or class.
 
-```<div class="sample-card">```
+```<div class="card-1">```
 
 Then ...
 
 ```
 
 makeCardUi({
-    class: 'sample-card',
+    class: 'card-1',
     value: 14,
     suit: 'S'
 })
 
 ```
-
 
 ## Options
 
@@ -62,5 +60,141 @@ makeCardUi({
 * *height* - The height of the card. *insert only number*
 * *width* - The width of the card. *insert only number*
 * *borderColor* - Border color.
-* *borderSize* - Border size.
+* *borderSize* - Border size *you can put 'none'*.
 * *radius* - Border raduis.
+
+# Create back side card 
+
+
+First we create a simple element with id or class.
+
+```<div id="card-1">```
+
+Then ...
+
+```
+makeBackCardUi({ 
+    id: 'card-1', 
+    src: 'img/img.jpg' 
+})
+
+```
+
+# Options
+
+```
+
+makeBackCardUi({
+    src: 'src image',
+    color: 'color'
+    id: 'id-name',
+    class: 'class-name',
+    height:Number,
+    width: Number,
+    borderColor: 'color'
+    borderSize: Number,
+    radius: Number, 
+})
+    
+```
+
+* *src* -Insert src image.
+* *color* - You can put background color instead of an image *do not put an image with a color* 
+* *id* - Insert the id of the element. *do not put a class with id*
+* *class* - Insert the class of the element. *do not put a class with id*
+* *height* - The height of the card. *insert only number*
+* *width* - The width of the card. *insert only number*
+* *borderColor* - Border color.
+* *borderSize* - * *borderSize* - Border size *you can put 'none'*.
+* *radius* - Border raduis.
+
+# Create back and front flip card
+
+First we create a simple element with id or class.
+
+```<div id="card-1">```
+
+Then ...
+
+```
+
+createCardFlip({ 
+    id: 'card-1' 
+    suit: 'c', 
+    value: 'k', 
+    src: 'img/img.jpg', 
+})
+
+```
+
+To create a flip card just pass the id or class to this function 
+
+`flipCard('card-1')`
+
+Sample to create a flip with click event
+
+```
+
+const card =  document.querySelector('#card-1')
+
+// create event
+card.addEventListener('click', e => {
+    flipCard('card-1')
+})
+
+```
+
+## options
+
+```
+
+createCardFlip({ 
+    id: 'card-1' 
+    value: Number,
+    suit: 'Suit',
+    src: 'src image',
+    backSideBorder: Number, 
+    frontSideBorder: Number, 
+    borderSize: Number, 
+    radius: Number, 
+    borderColor: 'color'
+})
+
+```
+
+*important* - All the options its the same like 'makeBackCardUi' and 'makeCardUi' execpt the borderSize. In This time we have front and back so we can choose individualy border size.
+*backSideBorder*
+*frontSideBorder*
+
+## Extra
+
+If some reason you want to create first a front card and just after logic to add the back card. 
+you can do it like this. 
+
+First we create a Flip card but this time we dont put the value and the suit.
+
+```createCardFlip({ id: 'card-1', src: 'img/img.jpg' })```
+
+Then we create event to the card
+
+```
+
+const card =  document.querySelector('#card-1')
+
+// create event
+card.addEventListener('click', e => {
+    flipCard('card-1')
+})
+
+```
+
+Then call this function with the id or class, value , suit. 
+
+```
+
+addBack({ id: 'card-1', value: 12, suit: 'h' })
+
+
+```
+
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function makeCardUi(option) {
-
+    console.log(3, option);
     let cardDiv;
     if (!option.class && !option.id) {
         console.error('You need to insert id element or class')
@@ -11,7 +11,7 @@ function makeCardUi(option) {
         if (option.class) {
             cardDiv = document.querySelector(`.${option.class}`);
         } else if (option.id) {
-            cardDiv = document.getElementById(`${option.id}`);
+            cardDiv = document.querySelector(`#${option.id}`);
         }
 
         if (cardDiv) {
@@ -74,18 +74,40 @@ function makeCardUi(option) {
             } else {
                 cardRadius = Number(option.radius) + 'px';
             }
-
-            if (option.borderSize == '' || option.borderSize == null || option.borderSize == undefined) {
-                cardBorderWidth = '2px';
+            console.log(77, option.borderFlip, option.borderSize);
+            if (option.borderSize > 0) {
+                if (option.borderSize == '' || option.borderSize == null || option.borderSize == undefined) {
+                    cardBorderWidth = '2px';
+                } else if (option.borderSize === 'none') {
+                    cardBorderWidth = null
+                } else {
+                    cardBorderWidth = Number(option.borderSize) + 'px';
+                }
             } else {
-                cardBorderWidth = Number(option.borderSize) + 'px';
+                if (option.borderFlip == '' || option.borderFlip == null || option.borderFlip == undefined) {
+                    cardBorderWidth = '2px';
+                } else if (option.borderFlip = 'none') {
+                    cardBorderWidth = null
+                } else {
+                    cardBorderWidth = Number(option.borderFlip) + 'px';
+                }
             }
+
+
+            console.log(96, cardBorderWidth);
 
             if (option.borderColor == '' || option.borderColor == null || option.borderColor == undefined) {
                 cardBorderColor = 'black';
             } else {
                 cardBorderColor = option.borderColor;
             }
+            let border;
+            if (cardBorderWidth === null) {
+                border = 'none'
+            } else {
+                border = `${cardBorderWidth} solid ${cardBorderColor}`
+            }
+
 
             if (value == 'J' || value == 'Q' || value == 'K' || value == 'j' || value == 'q' || value == 'k' || value == 'A' || value == 'a') {
                 cardDiv.innerText = value;
@@ -105,6 +127,7 @@ function makeCardUi(option) {
             let sizeAfterRoot = Number(splitHeight) / 7;
             let leftRightRoot = Number(splitHeight) / 32.81;
             let topLeftRoot = Number(splitHeight) / 32.81;
+            console.log(cardDiv);
 
             cardDiv.style.setProperty("--s-a", `${sizeAfterRoot}px`);
             cardDiv.style.setProperty("--s-c", `${sizeRoot}px`);
@@ -113,7 +136,7 @@ function makeCardUi(option) {
             cardDiv.style.setProperty("--h", cardHeight);
             cardDiv.style.setProperty("--w", cardWidth);
             cardDiv.style.setProperty("--b-r", cardRadius);
-            cardDiv.style.setProperty("--b", `${cardBorderWidth} solid ${cardBorderColor}`);
+            cardDiv.style.setProperty("--b", border);
             cardDiv.setAttribute('data-value', `${value}${simbol}`)
         } else {
             console.error('Something wrong with the class or id')
@@ -121,5 +144,238 @@ function makeCardUi(option) {
     }
 
 }
-makeCardUi({ value: 2, suit: 'C', class: "card" })
-module.exports.makeCardUi = makeCardUi;
+
+function makeBackCardUi(option) {
+    console.log(137, option);
+    let cardDiv;
+    if (!option.class && !option.id) {
+        console.error('You need to insert id element or class')
+
+    } else if (!option.src && !option.color) {
+        console.log(option.src, option.color);
+        console.log(1);
+        console.error('You need to insert img or background color')
+    } else {
+
+        if (option.class) {
+            cardDiv = document.querySelector(`.${option.class}`);
+        } else if (option.id) {
+            cardDiv = document.querySelector(`#${option.id}`);
+        }
+
+        if (cardDiv) {
+
+            cardDiv.classList.add('card-back', 'card-ui');
+            if (option.width > 0 && option.height == '' || option.width > 0 && option.height == null || option.width > 0 && option.height == undefined) {
+                cardWidth = Number(option.width) + 'px';
+                cardHeight = Number(option.width * 1.4) + 'px';
+            } else if (option.width == '' && option.height > 0 || option.width == null && option.height > 0 || option.width == undefined && option.height > 0) {
+                cardWidth = Number(option.height) + 'px';
+                cardHeight = Number(option.height * 1.4) + 'px';
+            } else if (option.width > 0 && option.height > 0 || option.width > 0 && option.height > 0 || option.width > 0 && option.height > 0) {
+                cardHeight = Number(option.height) + 'px';
+                cardWidth = Number(option.width) + 'px';
+            } else if (option.width == '' && option.height == '' || option.width == null && option.height == null || option.width == undefined && option.height == undefined) {
+                cardHeight = '210px';
+                cardWidth = '150px';
+            }
+
+            if (option.radius == '' || option.radius == null || option.radius == undefined) {
+                cardRadius = '5px';
+            } else {
+                cardRadius = Number(option.radius) + 'px';
+            }
+
+            if (option.borderSize > 0) {
+                if (option.borderSize == '' || option.borderSize == null || option.borderSize == undefined) {
+                    cardBorderWidth = '2px';
+                } else if (option.borderSize === 'none') {
+                    cardBorderWidth = null
+                } else {
+                    cardBorderWidth = Number(option.borderSize) + 'px';
+                }
+            } else {
+                if (option.borderFlipFront == '' || option.borderFlipFront == null || option.borderFlipFront == undefined) {
+                    cardBorderWidth = '2px';
+                } else if (option.borderFlipFront = 'none') {
+                    cardBorderWidth = null
+                } else {
+                    cardBorderWidth = Number(option.borderFlipFront) + 'px';
+                }
+            }
+            console.log(205, option.borderFlipFront, option.borderSize);
+            console.log(cardBorderWidth);
+            if (option.borderColor == '' || option.borderColor == null || option.borderColor == undefined) {
+                cardBorderColor = 'black';
+            } else {
+                cardBorderColor = option.borderColor;
+            }
+            let border;
+            if (cardBorderWidth === null) {
+                border = 'none'
+            } else {
+                border = `${cardBorderWidth} solid ${cardBorderColor}`
+            }
+            console.log(border);
+            let splitHeight = cardHeight.split("px")[0]
+            let sizeRoot = Number(splitHeight) / 2.3;
+            let sizeAfterRoot = Number(splitHeight) / 7;
+            let leftRightRoot = Number(splitHeight) / 32.81;
+            let topLeftRoot = Number(splitHeight) / 32.81;
+            console.log(cardDiv);
+            cardDiv.style.setProperty("--s-a", `${sizeAfterRoot}px`);
+            cardDiv.style.setProperty("--s-c", `${sizeRoot}px`);
+            cardDiv.style.setProperty("--l-r", `${leftRightRoot}px`);
+            cardDiv.style.setProperty("--t-p", `${topLeftRoot}px`);
+            cardDiv.style.setProperty("--h", cardHeight);
+            cardDiv.style.setProperty("--w", cardWidth);
+            cardDiv.style.setProperty("--b-r", cardRadius);
+            cardDiv.style.setProperty("--b", border);
+            if (option.src) {
+                cardDiv.style.background = `url(${option.src})`;
+                cardDiv.style.backgroundSize = `100% 100%`
+            } else if (option.color) {
+                cardDiv.style.background = option.color;
+            }
+
+        } else {
+            console.error('Something wrong with the class or id')
+        }
+    }
+}
+
+function flipCard(Card) {
+    let card = document.querySelector(`.${Card} .card-flip-inner`);
+    if (!card) {
+        card = document.querySelector(`#${Card} .card-flip-inner`);
+    }
+    if (card.querySelector('.card-face .card-sample-back .card-ui')) {
+        card.classList.toggle('is-flipped');
+    }
+}
+
+function createCardFlip(option) {
+    if (!option.class && !option.id) {
+        console.error('You need to insert id element or class')
+
+    } else {
+        console.log(239, option);
+        let temp = `<div class="card-flip-inner">
+                        <div class="card-face card-face-front">
+                            <div class="card-sample">
+                            <div class="front"></div>
+                            </div>
+                        </div>
+                        <div class="card-face card-face-back">
+                            <div class="card-sample-back">
+                            <div class="back"></div>
+                            </div>
+                        </div>
+                    </div>`;
+        let CARD;
+        if (option.class) {
+
+            CARD = document.querySelector(`.${option.class}`);
+        } else if (option.id) {
+
+            CARD = document.getElementById(option.id);
+        }
+
+        let newCard = document.createElement('div')
+        newCard.className = 'card-flip';
+        CARD.appendChild(newCard);
+        newCard.innerHTML = temp;
+        if (option.id) {
+            console.log(266, option.borderSize);
+
+            makeBackCardUi({
+                src: option.src,
+                id: `${option.id} .card-flip .card-flip-inner .card-face .card-sample .front`,
+                width: option.width,
+                height: option.height,
+                borderColor: option.borderColor,
+                borderSize: option.frontSideBorder,
+                borderFlipFront: option.frontSideBorder,
+                radius: option.radius,
+                color: option.color
+            })
+        } else if (option.class) {
+            // console.log(266, option.borderSize);
+            makeBackCardUi({
+                src: option.src,
+                class: `${option.class} .card-flip .card-flip-inner .card-face .card-sample .front`,
+                width: option.width,
+                height: option.height,
+                borderColor: option.borderColor,
+                borderSize: option.frontSideBorder,
+                borderFlipFront: option.frontSideBorder,
+                radius: option.radius,
+                color: option.color
+            })
+        }
+
+        if (option.suit || option.value) {
+            console.log(option, 272);
+            if (option.class) {
+                console.log(option, 272);
+
+                makeCardUi({
+                    value: option.value,
+                    suit: option.suit,
+                    class: `${option.class} .card-flip .card-flip-inner .card-face .card-sample-back .back`,
+                    width: option.width, height: option.height,
+                    borderColor: option.borderColor,
+                    borderSize: option.backSideBorder,
+                    borderFlip: option.backSideBorder,
+                    radius: option.radius
+                })
+            } else if (option.id) {
+                console.log(option, 272);
+
+                makeCardUi({
+                    value: option.value,
+                    suit: option.suit,
+                    id: `${option.id} .card-flip .card-flip-inner .card-face .card-sample-back .back`,
+                    width: option.width,
+                    height: option.height,
+                    borderColor: option.borderColor,
+                    borderSize: option.backSideBorder,
+                    borderFlip: option.backSideBorder,
+                    radius: option.radius
+                })
+            }
+        }
+        let cardUi;
+        if (option.class) {
+            cardUi = document.querySelector(`.${option.class} .card-flip .card-flip-inner .card-face .card-sample .front`);
+        } else if (option.id) {
+            cardUi = document.querySelector(`#${option.id} .card-flip .card-flip-inner .card-face .card-sample .front`);
+        }
+        console.log(newCard);
+        newCard.style.width = getComputedStyle(cardUi).getPropertyValue("--w")
+        newCard.style.height = getComputedStyle(cardUi).getPropertyValue("--h")
+        newCard.style.borderRadius = getComputedStyle(cardUi).getPropertyValue("--b-r")
+        // newCard.style.border = getComputedStyle(cardUi).getPropertyValue("--b")
+        // console.log(getComputedStyle(cardUi).getPropertyValue("--b"));
+    }
+
+
+}
+
+function addBack(option) {
+    let cardUi;
+    if (option.id) {
+        cardUi = document.querySelector(`#${option.id} .card-flip .card-flip-inner .card-face .card-sample .front`);
+    } else if (option.class) {
+        cardUi = document.querySelector(`.${option.class} .card-flip .card-flip-inner .card-face .card-sample .front`);
+    }
+    let width = getComputedStyle(cardUi).getPropertyValue("--w").split("px")[0]
+    let borderRadius = getComputedStyle(cardUi).getPropertyValue("--b-r").split("px")[0];
+    if (option.id) {
+        makeCardUi({ value: option.value, suit: option.suit, id: `${option.id} .card-flip .card-flip-inner .card-face .card-sample-back .back`, width: width, radius: borderRadius })
+    } else if (option.class) {
+        makeCardUi({ value: option.value, suit: option.suit, class: `${option.class} .card-flip .card-flip-inner .card-face .card-sample-back .back`, width: width, radius: borderRadius })
+    }
+}
+
+module.exports = { makeCardUi, makeBackCardUi, createCardFlip, flipCard, addBack };
